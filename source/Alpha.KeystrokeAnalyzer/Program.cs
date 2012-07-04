1e7ec85befcf5d64d2630e4cb87ebb3e5f36cb2a
Alpha <alphagma@gmail.com>
2012-07-04 12:00:26 -0500

    Added POC for HookLib integration

--- source/Alpha.KeystrokeAnalyzer/Program.cs
+++ source/Alpha.KeystrokeAnalyzer/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using HookLib;
+using HookLib.Windows;
 
 namespace Alpha.KeystrokeAnalyzer
 {
@@ -10,6 +12,25 @@ namespace Alpha.KeystrokeAnalyzer
     {
         static void Main(string[] args)
         {
+            var hookManager = new HookManager();
+            hookManager.KeyPressed += hookManager_KeyPressed;
+            hookManager.MouseClick += hookManager_MouseClick;
+
+            var messagePump = new WindowsMessagePump();
+            messagePump.Run();
+
+            System.Console.WriteLine("Finished.");
+            System.Console.ReadKey();
+        }
+
+        static void hookManager_MouseClick(object sender, GlobalMouseEventHandlerArgs args)
+        {
+            Console.WriteLine(args.Point);
+        }
+
+        static void hookManager_KeyPressed(object sender, GlobalKeyEventHandlerArgs args)
+        {
+            Console.WriteLine(args.Character);
         }
     }
 }
